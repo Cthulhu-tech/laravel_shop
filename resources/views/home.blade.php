29357feeb0cfc605ad91c0ee235bb0285f5b02aa
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-11 00:37:05 +0400

    views home add

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Shop')
+@section('css', 'css/home/home.css')
+@section('content')
+    <section>
+        <article>
+            <h1 class="title">GOOD SHOP</h1>
+            <p></p>
+        </article>
+    </section>
+@endsection
