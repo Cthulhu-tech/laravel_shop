94f1cb53f987949bad6a9b42f34519da67ee3f53
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-11 00:36:44 +0400

    slider add

---
+++ resources/views/components/slider/slider.blade.php
@@ -0,0 +1,11 @@
+@section('js', 'js/slider/slider.js')
+
+<section class="slider">
+        <div class="slider-btn slider-btn_left"></div>
+        <div class="slider-line">
+            <img src="" alt="" class="slider-img">
+            <img src="" alt="" class="slider-img">
+            <img src="" alt="" class="slider-img">
+        </div>
+        <div class="slider-btn slider-btn_right"></div>
+</section>
